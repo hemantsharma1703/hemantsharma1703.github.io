4a3603aeca99380c6cf382c94ff997355642ae1d
Hemant <itshemantsharma173@gmail.com>
2022-10-21 20:36:01 +0530

    refined responsive menu-bar

--- sources/scripts/utility.js
+++ sources/scripts/utility.js
@@ -1,9 +1,17 @@
 function toggleMenu() {
     let navbar = document.getElementById('menu');
+    let menubutton = document.getElementById('menubtn');
+    let closebutton = document.getElementById('closebtn')
     if(window.innerWidth <= 900) {
-        if(navbar.style.display != 'block')
+        if(navbar.style.display != 'block') {
             navbar.style.display = 'block';
-        else
-            navbar.style.display = 'none';
+            menubutton.style.display = 'none';
+            closebutton.style.display = 'block';
+        }
+        else {
+            navbar.style.display = 'none'; 
+            menubutton.style.display = 'block';
+            closebutton.style.display = 'none';
+        }
     }
 }
