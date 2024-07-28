5b3da7e58e08fa62997e3edca78b9476bd00f117
Hemant Sharma <itshemantsharma173@gmail.com>
2024-07-28 14:35:03 +0530

    portfolio complete

--- src/app/lib/blogs/posts.ts
+++ src/app/lib/blogs/posts.ts
@@ -7,9 +7,44 @@ import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import rehypeFormat from "rehype-format";
 import rehypeRaw from "rehype-raw";
+import rehypeHighlight from "rehype-highlight";
 import { visit } from "unist-util-visit";
 
 const postsDirectory = path.join(process.cwd(), "/src/app/lib/blogs/");
+
+function remarkImageRoot() {
+  return (tree: any) => {
+    visit(tree, "image", (node) => {
+      node.url = `/` + node.url.split("/").pop();
+    });
+  };
+}
+
+function rehypeTailwindClasses() {
+  return (tree: any) => {
+    visit(tree, "element", (node) => {
+      const tailwindClasses: { [key: string]: string } = {
+        h1: "text-4xl font-bold my-4",
+        h2: "text-3xl font-semibold border-b-2 border-neutral-900 mt-6 leading-10",
+        p: "mb-10 mt-5 leading-7 text-lg text-neutral-200",
+        ul: "list-disc list-inside my-4",
+        ol: "list-decimal list-inside my-4",
+        li: "my-1",
+        blockquote:
+          "border-l-4 border-neutral-900 bg-neutral-800 text-neutral-100 p-4 my-4",
+        // code: "bg-neutral-700 rounded-lg px-3 text-neutral-100",
+        pre: "bg-neutral-900 rounded-xl p-4 my-4",
+        a: "text-[#2997ff] hover:text-[#43B9B9] active:opacity-50 underline visited:text-purple-500 hover:visited:text-purple-700",
+      };
+      const className = tailwindClasses[node.tagName];
+      if (className) {
+        node.properties.className = (node.properties.className || []).concat(
+          className
+        );
+      }
+    });
+  };
+}
 
 export function getSortedPostsData() {
   const fileNames = fs.readdirSync(postsDirectory);
@@ -36,27 +71,6 @@ export function getSortedPostsData() {
   });
 }
 
-function rehypeTailwindClasses() {
-  return (tree: any) => {
-    visit(tree, "element", (node) => {
-      const tailwindClasses: { [key: string]: string } = {
-        h1: "text-4xl font-bold my-4",
-        h2: "text-3xl font-semibold my-3",
-        p: "my-2",
-        code: "bg-neutral-700 rounded",
-        pre: "bg-neutral-900 rounded-xl p-4 my-4",
-        a: "text-[#2997ff] hover:text-[#43B9B9] active:opacity-50 underline visited:text-purple-500 hover:visited:text-purple-700",
-      };
-      const className = tailwindClasses[node.tagName];
-      if (className) {
-        node.properties.className = (node.properties.className || []).concat(
-          className
-        );
-      }
-    });
-  };
-}
-
 export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
@@ -65,9 +79,11 @@ export async function getPostData(id: string) {
   if (matterResult) {
     const processedContent = await unified()
       .use(remarkParse)
+      .use(remarkImageRoot)
       .use(remarkRehype, { allowDangerousHtml: true })
       .use(rehypeRaw)
       .use(rehypeTailwindClasses)
+      .use(rehypeHighlight)
       .use(rehypeFormat)
       .use(rehypeStringify)
       .process(matterResult.content);
